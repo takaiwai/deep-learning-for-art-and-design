4e747e6e427a3aed510771a7ef9e7abb52a55c38
Taka Iwai <hello@takaiwai.com>
2017-12-31 12:10:51 -0800

    Implement predict and loss

--- code/BasicNet.py
+++ code/BasicNet.py
@@ -1,24 +1,47 @@
 import numpy as np
 from lib.MNIST import MNIST
+from lib.functions import sigmoid, softmax, cross_entropy
 
 class BasicNet:
     def __init__(self):
         print("Created BasicNet instance")
-        self.mnist = MNIST()
 
-        train_images, train_labels, test_images, test_labels = self.mnist.get_dataset()
-        print(train_images.shape)
-        print(train_labels.shape)
-        print(test_images.shape)
-        print(test_labels.shape)
+        SIGMA = 0.01
+        self.params = {}
+        self.params['W1'] = np.random.randn(28*28, 32) * SIGMA
+        self.params['b1'] = np.random.rand(32) * SIGMA
+        self.params['W2'] = np.random.randn(32, 10) * SIGMA
+        self.params['b2'] = np.random.rand(10) * SIGMA
 
-        self.mnist.show(train_images[0], train_labels[0])
-        self.mnist.show(train_images[10], train_labels[10])
-        self.mnist.show(test_images[0], test_labels[0])
-        self.mnist.show(test_images[10], test_labels[10])
+    def predict(self, X):
+        W1, W2 = self.params['W1'], self.params['W2']
+        b1, b2 = self.params['b1'], self.params['b2']
 
+        Z1 = np.dot(X, W1) + b1
+        A1 = sigmoid(Z1)
+        Z2 = np.dot(A1, W2) + b2
+        Y = softmax(Z2)
+
+        return Y
+
+    def loss(self, X, T):
+        Y = self.predict(X)
+        loss = cross_entropy(Y, T)
+
+        return loss
 
 
 if __name__ == '__main__':
     print("this is main")
     basic_net = BasicNet()
+
+    mnist = MNIST()
+    train_images, train_labels, test_images, test_labels = mnist.get_dataset()
+
+    prediction = basic_net.predict(test_images[:5])
+    print(prediction)
+
+    loss = basic_net.loss(test_images[:5], test_labels[:5])
+    print(loss)
+
+
