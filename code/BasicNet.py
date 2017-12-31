2d887a5cae5b07a0d7587f83de6c95f55ec68196
Taka Iwai <hello@takaiwai.com>
2017-12-31 11:15:51 -0800

    Create BasicNet and MNIST class

---
+++ code/BasicNet.py
@@ -0,0 +1,18 @@
+import numpy as np
+from lib.MNIST import MNIST
+
+class BasicNet:
+    def __init__(self):
+        print("basic net")
+        print(MNIST())
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    basic_net = BasicNet()
+    print("this is main")
---
+++ lib/MNIST.py
@@ -0,0 +1,4 @@
+
+class MNIST:
+    def __init__(self):
+        print("This is MNIST class")
