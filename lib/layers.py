b60d11b6714d681008f225dc168ac5ebe6c9de44
Taka Iwai <hello@takaiwai.com>
2017-12-31 14:34:42 -0800

    Add layers.py

---
+++ lib/layers.py
@@ -0,0 +1,18 @@
+import sys, os
+sys.path.append(os.pardir)
+from lib.functions import sigmoid, softmax, cross_entropy
+
+class MatMulLayer():
+    pass
+
+class MatAddLayer():
+    pass
+
+class DenseLayer():
+    pass
+
+class SigmoidLayer():
+    pass
+
+class SoftmaxCrossEntropyLayer():
+    pass
