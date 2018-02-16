0628a0373bee8751feba62ef445efe359b5b9897
Taka Iwai <hello@takaiwai.com>
2018-02-15 20:32:48 -0800

    Run seven layer net 5 times

--- code/08_techniques/08_9_ensemble_learning/08_9_1_better_seven_layer.py
+++ code/08_techniques/08_9_ensemble_learning/08_9_1_better_seven_layer.py
@@ -3,72 +3,89 @@ sys.path.append(path.abspath(path.join(__file__ ,"../../../..")))
 import numpy as np
 import pickle
 from lib.MNIST import MNIST
-from lib.layers import DenseLayer, BatchNormLayer, ReluLayer, SoftmaxCrossEntropyLayer, SigmoidLayer
+from lib.layers import DenseLayer, ReluLayer, SoftmaxCrossEntropyLayer
 from collections import OrderedDict
 
-class BatchNormNet:
-    def __init__(self, weight_stddev=0.01, uses_batch_norm=False, experiment_num=0):
-        self.params = None
+def he(n_in):
+    return np.sqrt(2 / n_in)
+
+class AdamOptimizer:
+    def __init__(self, params, ETA=0.001, BETA1 = 0.9, BETA2 = 0.999, EPSILON = 1e-8):
+        self.iteration = 0
+        self.v = {}
+        self.s = {}
+        self.v_corrected = {}
+        self.s_corrected = {}
+
+        for key, param in params.items():
+            self.v[key] = np.zeros_like(param)
+            self.s[key] = np.zeros_like(param)
+            self.v_corrected[key] = np.zeros_like(param)
+            self.s_corrected[key] = np.zeros_like(param)
+
+        self.ETA = ETA
+        self.BETA1 = BETA1
+        self.BETA2 = BETA2
+        self.EPSILON = EPSILON
+
+    def update(self, params, grads):
+        c_beta1 = self.BETA1 ** (self.iteration + 1)
+        c_beta2 = self.BETA2 ** (self.iteration + 1)
+        self.iteration += 1
+
+        for key in self.v.keys():
+            self.v[key] = self.BETA1 * self.v[key] + (1 - self.BETA1) * grads[key]
+            self.v_corrected[key] = self.v[key] / (1 - c_beta1)
+
+        for key in self.s.keys():
+            self.s[key] = self.BETA2 * self.s[key] + (1 - self.BETA2) * (grads[key] ** 2)
+            self.s_corrected[key] = self.s[key] / (1 - c_beta2)
+
+        for key in params.keys():
+            params[key] -= self.ETA * self.v_corrected[key] / np.sqrt(self.s_corrected[key] + self.EPSILON)
+
+
+
+class SevenLayerNet:
+    def __init__(self):
+        self.params = {}
         self.layers = None
-        
-        self.weight_stddev = weight_stddev
-        self.uses_batch_norm = uses_batch_norm
-        self.experiment_num = experiment_num
 
         self.init_params()
         self.init_layers()
 
+        self.optimizer = AdamOptimizer(self.params, 0.001)
+
     def init_params(self):
-        self.params = {}
-        self.params['W1'] = np.random.randn(28*28, 64) * self.weight_stddev
+        self.params['W1'] = np.random.randn(28*28, 64) * he(28*28)
         self.params['b1'] = np.zeros(64)
-        if self.uses_batch_norm:
-            self.params['gamma1'] = np.ones(64)
-            self.params['beta1'] = np.zeros(64)
-
-        self.params['W2'] = np.random.randn(64, 50) * self.weight_stddev
+        self.params['W2'] = np.random.randn(64, 50) * he(64)
         self.params['b2'] = np.zeros(50)
-        if self.uses_batch_norm:
-            self.params['gamma2'] = np.ones(50)
-            self.params['beta2'] = np.zeros(50)
-
-        self.params['W3'] = np.random.randn(50, 40) * self.weight_stddev
+        self.params['W3'] = np.random.randn(50, 40) * he(50)
         self.params['b3'] = np.zeros(40)
-        if self.uses_batch_norm:
-            self.params['gamma3'] = np.ones(40)
-            self.params['beta3'] = np.zeros(40)
-        
-        self.params['W4'] = np.random.randn(40, 30) * self.weight_stddev
+        self.params['W4'] = np.random.randn(40, 30) * he(40)
         self.params['b4'] = np.zeros(30)
-        if self.uses_batch_norm:
-            self.params['gamma4'] = np.ones(30)
-            self.params['beta4'] = np.zeros(30)
-        
-        self.params['W7'] = np.random.randn(30, 10) * self.weight_stddev
+        self.params['W5'] = np.random.randn(30, 30) * he(30)
+        self.params['b5'] = np.zeros(30)
+        self.params['W6'] = np.random.randn(30, 30) * he(30)
+        self.params['b6'] = np.zeros(30)
+        self.params['W7'] = np.random.randn(30, 10) * he(30)
         self.params['b7'] = np.zeros(10)
 
     def init_layers(self):
         self.layers = OrderedDict()
         self.layers['Dense1'] = DenseLayer(self.params['W1'], self.params['b1'])
-        if self.uses_batch_norm:
-            self.layers['BatchNorm1'] = BatchNormLayer(self.params['gamma1'], self.params['beta1'])
         self.layers['Relu1'] = ReluLayer()
-
         self.layers['Dense2'] = DenseLayer(self.params['W2'], self.params['b2'])
-        if self.uses_batch_norm:
-            self.layers['BatchNorm2'] = BatchNormLayer(self.params['gamma2'], self.params['beta2'])
         self.layers['Relu2'] = ReluLayer()
-
         self.layers['Dense3'] = DenseLayer(self.params['W3'], self.params['b3'])
-        if self.uses_batch_norm:
-            self.layers['BatchNorm3'] = BatchNormLayer(self.params['gamma3'], self.params['beta3'])
         self.layers['Relu3'] = ReluLayer()
-        
         self.layers['Dense4'] = DenseLayer(self.params['W4'], self.params['b4'])
-        if self.uses_batch_norm:
-            self.layers['BatchNorm4'] = BatchNormLayer(self.params['gamma4'], self.params['beta4'])
         self.layers['Relu4'] = ReluLayer()
-        
+        self.layers['Dense5'] = DenseLayer(self.params['W5'], self.params['b5'])
+        self.layers['Relu5'] = ReluLayer()
+        self.layers['Dense6'] = DenseLayer(self.params['W6'], self.params['b6'])
+        self.layers['Relu6'] = ReluLayer()
         self.layers['Dense7'] = DenseLayer(self.params['W7'], self.params['b7'])
         self.last_layer = SoftmaxCrossEntropyLayer()
 
@@ -100,10 +117,8 @@ class BatchNormNet:
         return np.mean(Z_index == T_index)
 
     def gradient_descent(self, X, T):
-        ETA = 0.1
-        grads = self.gradients(X, T)
-        for param_name in list(self.params.keys()):
-            self.params[param_name] -= ETA * grads[param_name]
+        grads = seven_layer_net.gradients(X, T)
+        self.optimizer.update(self.params, grads)
 
     def gradients(self, X, T):
         self.loss(X, T)
@@ -123,18 +138,12 @@ class BatchNormNet:
         gradients['b3'] = self.layers['Dense3'].db
         gradients['W4'] = self.layers['Dense4'].dW
         gradients['b4'] = self.layers['Dense4'].db
+        gradients['W5'] = self.layers['Dense5'].dW
+        gradients['b5'] = self.layers['Dense5'].db
+        gradients['W6'] = self.layers['Dense6'].dW
+        gradients['b6'] = self.layers['Dense6'].db
         gradients['W7'] = self.layers['Dense7'].dW
         gradients['b7'] = self.layers['Dense7'].db
-
-        if self.uses_batch_norm:
-            gradients['gamma1'] = self.layers['BatchNorm1'].dgamma
-            gradients['beta1'] = self.layers['BatchNorm1'].dbeta
-            gradients['gamma2'] = self.layers['BatchNorm2'].dgamma
-            gradients['beta2'] = self.layers['BatchNorm2'].dbeta
-            gradients['gamma3'] = self.layers['BatchNorm3'].dgamma
-            gradients['beta3'] = self.layers['BatchNorm3'].dbeta
-            gradients['gamma4'] = self.layers['BatchNorm4'].dgamma
-            gradients['beta4'] = self.layers['BatchNorm4'].dbeta
 
         return gradients
 
@@ -143,7 +152,6 @@ class BatchNormNet:
 
         gradients = {}
         for param_name in list(self.params.keys()):
-            print("calculating: ", param_name)
             gradients[param_name] = self.numerical_gradient(loss, self.params[param_name])
 
         return gradients
@@ -185,25 +193,16 @@ class BatchNormNet:
             else:
                 result = 'NG'
 
-            print("========== {}".format(key))
-            print(diff)
-            print(prop)
             print("gradient {}: {} ({})".format(key, result, check))
 
-def experiment(weight_stddev, uses_batch_norm, experiment_num):
-    batch_norm_net = BatchNormNet(weight_stddev, uses_batch_norm, experiment_num)
+
+if __name__ == '__main__':
+    print("this is main")
+
+    seven_layer_net = SevenLayerNet()
 
     mnist = MNIST()
     train_images, train_labels, test_images, test_labels = mnist.get_dataset()
-
-    log = {
-        'accuracy_train': [],
-        'accuracy_train_itr': [],
-        'accuracy_test': [],
-        'accuracy_test_itr': [],
-        'weight_stddev': weight_stddev,
-        'uses_batch_norm': uses_batch_norm
-    }
 
     epochs = 5
     train_size = train_images.shape[0]
@@ -215,48 +214,25 @@ def experiment(weight_stddev, uses_batch_norm, experiment_num):
     for epoch in range(epochs):
         print("Epoch: {}".format(epoch))
 
-        batch_norm_net.is_training = True
         for _ in range(iteration_per_epoch):
-
-            if itr % 60 == 0:
-                batch_norm_net.is_training = False
-                train_acc = batch_norm_net.accuracy(train_images, train_labels)
-                test_acc = batch_norm_net.accuracy(test_images, test_labels)
-                log['accuracy_train'].append(train_acc)
-                log['accuracy_train_itr'].append(itr)
-                log['accuracy_test'].append(test_acc)
-                log['accuracy_test_itr'].append(itr)
-                print("[Accuracy] train: {}, test: {}".format(train_acc, test_acc))
-                batch_norm_net.is_training = True
-
             batch_mask = np.random.choice(train_size, batch_size)
             batch_images = train_images[batch_mask]
             batch_labels = train_labels[batch_mask]
+            seven_layer_net.gradient_descent(batch_images, batch_labels)
 
-            batch_norm_net.gradient_descent(batch_images, batch_labels)
             itr += 1
 
-        batch_norm_net.is_training = False
 
     print("Done!")
 
-    train_acc = batch_norm_net.accuracy(train_images, train_labels)
-    test_acc = batch_norm_net.accuracy(test_images, test_labels)
-    log['accuracy_train'].append(train_acc)
-    log['accuracy_train_itr'].append(itr)
-    log['accuracy_test'].append(test_acc)
-    log['accuracy_test_itr'].append(itr)
+    train_acc = seven_layer_net.accuracy(train_images, train_labels)
+    test_acc = seven_layer_net.accuracy(test_images, test_labels)
     print("[Accuracy] train: {}, test: {}".format(train_acc, test_acc))
 
-    pickle.dump(log, open(path.join(path.dirname(__file__ ), 'experiment_{}_{}.pkl'.format(experiment_num, uses_batch_norm)), "wb"))
+    log = {
+        'train_acc': train_acc,
+        'test_acc': test_acc
+    }
+    pickle.dump(log, open(path.join(path.dirname(__file__ ), 'seven_layer_5_log.pkl'), "wb"))
 
-if __name__ == '__main__':
-    print("this is main")
-    np.random.seed(1229)
-
-    for i in range(16):
-        r = -3 * np.random.rand()
-        weight_stddev = 10 ** r
-        print("======== stddev: ", weight_stddev)
-        experiment(weight_stddev, True, i)
-        experiment(weight_stddev, False, i)
+    seven_layer_net.save_params('seven_layer_5_params.pkl')
